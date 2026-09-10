8525608c3a6234c9515b61812a4da4b8c6fafbf4
Mikalai Tsyhankou <endogenesis1@gmail.com>
2026-09-10 14:41:25 +0200

    Fix shader ordering and category grouping

--- IOSMetalShaders/ShaderDemo.swift
+++ IOSMetalShaders/ShaderDemo.swift
@@ -72,6 +72,93 @@ enum ShaderDemo: Int, CaseIterable, Identifiable {
     case shadowsHighlights
     case colorBandShift
     case tritone
+    case timeDisplacement
+    case blueprint
+    case exposure
+    case displacementNoise
+    case paperGrain
+    case plasmaField
+    case gaussianBlur
+    case auroraGradient
+    case mosaic
+    case datamosh
+    case liquidGlass
+    case swirlTunnel
+    case cyberpunkGlitch
+    case bloom
+    case pincushionDistortion
+    case radialBlur
+    case checkerPattern
+    case dataCorruption
+    case softGlow
+    case rgbSplit
+    case contrast
+    case noiseWarp
+    case emboss
+    case scanlineJitter
+    case rainGlass
+    case unsharpMask
+    case channelDrift
+    case fluidGradient
+    case pinch
+    case waveInterference
+    case lumaKey
+    case bulge
+    case digitalStatic
+    case circleReveal
+    case sepia
+    case snowfall
+    case polarWarp
+    case lightLeak
+    case colorQuantizationGlitch
+    case godRays
+    case flagDistortion
+    case analogStatic
+    case stuckPixels
+    case terminalGlitch
+    case dotMatrix
+    case signalNoise
+    case metaballs
+    case posterize
+    case radialWipe
+    case lineDropout
+    case levels
+    case ghosting
+    case doubleVision
+    case horizontalTear
+    case compressionArtifacts
+    case chromaKey
+    case fisheye
+    case deadPixels
+    case verticalTear
+    case kaleidoscope
+    case glitchMosaic
+    case heatHaze
+    case sliceGlitch
+    case colorBalance
+    case toonShade
+    case vhsHeadSwitch
+    case pixelate
+    case vibrance
+    case noiseDissolve
+    case interlacing
+    case lensRefraction
+    case displacementTransition
+    case saturation
+    case mirror
+    case temperature
+    case bitCrush
+    case outline
+    case threshold
+    case fragmentShuffle
+    case twirl
+    case thermalVision
+    case frameJitter
+    case crtPhosphorMask
+    case linearWipe
+    case waterCaustics
+    case crtCurvature
+    case brokenLCD
 
     var id: Self { self }
 
@@ -83,30 +170,68 @@ enum ShaderDemo: Int, CaseIterable, Identifiable {
              .channelSwap, .vhsTracking, .echoFrames, .colorBandShift:
             .glitch
 
+        case .timeDisplacement, .displacementNoise, .datamosh, .cyberpunkGlitch,
+             .dataCorruption, .rgbSplit, .noiseWarp, .scanlineJitter, .channelDrift,
+             .waveInterference, .digitalStatic, .colorQuantizationGlitch,
+             .analogStatic, .stuckPixels, .terminalGlitch, .signalNoise,
+             .lineDropout, .ghosting, .doubleVision, .horizontalTear,
+             .compressionArtifacts, .deadPixels, .verticalTear, .glitchMosaic,
+             .sliceGlitch, .vhsHeadSwitch, .interlacing, .bitCrush,
+             .fragmentShuffle, .frameJitter, .crtPhosphorMask, .crtCurvature,
+             .brokenLCD:
+            .glitch
+
         case .grayscale, .hueRotation, .duotone, .colorInvert, .brightness, .tint,
              .gamma, .solarize, .colorCurves, .shadowsHighlights, .tritone:
+            .colorAndTone
+
+        case .exposure, .contrast, .sepia, .posterize, .levels, .colorBalance,
+             .vibrance, .saturation, .temperature, .threshold:
             .colorAndTone
 
         case .hexBokeh, .sharpen, .tiltShift, .vignette, .boxBlur,
              .directionalBlur, .zoomBlur, .motionBlur, .lensFlare:
             .blurAndLight
 
+        case .gaussianBlur, .bloom, .radialBlur, .softGlow, .unsharpMask,
+             .lightLeak, .godRays:
+            .blurAndLight
+
         case .wave, .pageCurl, .waterDroplets, .perspectiveTilt, .ripple,
              .magnifyingGlass, .spherize, .barrelDistortion:
+            .distortion
+
+        case .liquidGlass, .swirlTunnel, .pincushionDistortion, .pinch, .bulge,
+             .polarWarp, .flagDistortion, .fisheye, .kaleidoscope, .heatHaze,
+             .lensRefraction, .mirror, .twirl:
             .distortion
 
         case .frostedNoise, .halftone, .bayerDither, .filmGrain, .blueNoiseDither,
              .voronoiCells, .canvasTexture:
             .pixelAndTexture
 
+        case .paperGrain, .mosaic, .checkerPattern, .dotMatrix, .pixelate:
+            .pixelAndTexture
+
         case .oilPaint, .pencilSketch, .crosshatch, .sobelEdges, .watercolor,
              .laplacianEdges, .stipple:
+            .stylizationAndEdges
+
+        case .blueprint, .emboss, .toonShade, .outline, .thermalVision:
             .stylizationAndEdges
 
         case .burnDissolve, .pixelTransition, .irisReveal:
             .transitionsAndMasks
 
+        case .lumaKey, .circleReveal, .radialWipe, .chromaKey, .noiseDissolve,
+             .displacementTransition, .linearWipe:
+            .transitionsAndMasks
+
         case .fire, .starfield, .smoke:
+            .proceduralAndAnimated
+
+        case .plasmaField, .auroraGradient, .rainGlass, .fluidGradient, .snowfall,
+             .metaballs, .waterCaustics:
             .proceduralAndAnimated
         }
     }
@@ -242,6 +367,180 @@ enum ShaderDemo: Int, CaseIterable, Identifiable {
             ColorBandShiftDemoView()
         case .tritone:
             TritoneDemoView()
+        case .timeDisplacement:
+            TimeDisplacementDemoView()
+        case .blueprint:
+            BlueprintDemoView()
+        case .exposure:
+            ExposureDemoView()
+        case .displacementNoise:
+            DisplacementNoiseDemoView()
+        case .paperGrain:
+            PaperGrainDemoView()
+        case .plasmaField:
+            PlasmaFieldDemoView()
+        case .gaussianBlur:
+            GaussianBlurDemoView()
+        case .auroraGradient:
+            AuroraGradientDemoView()
+        case .mosaic:
+            MosaicDemoView()
+        case .datamosh:
+            DatamoshDemoView()
+        case .liquidGlass:
+            LiquidGlassDemoView()
+        case .swirlTunnel:
+            SwirlTunnelDemoView()
+        case .cyberpunkGlitch:
+            CyberpunkGlitchDemoView()
+        case .bloom:
+            BloomDemoView()
+        case .pincushionDistortion:
+            PincushionDistortionDemoView()
+        case .radialBlur:
+            RadialBlurDemoView()
+        case .checkerPattern:
+            CheckerPatternDemoView()
+        case .dataCorruption:
+            DataCorruptionDemoView()
+        case .softGlow:
+            SoftGlowDemoView()
+        case .rgbSplit:
+            RGBSplitDemoView()
+        case .contrast:
+            ContrastDemoView()
+        case .noiseWarp:
+            NoiseWarpDemoView()
+        case .emboss:
+            EmbossDemoView()
+        case .scanlineJitter:
+            ScanlineJitterDemoView()
+        case .rainGlass:
+            RainGlassDemoView()
+        case .unsharpMask:
+            UnsharpMaskDemoView()
+        case .channelDrift:
+            ChannelDriftDemoView()
+        case .fluidGradient:
+            FluidGradientDemoView()
+        case .pinch:
+            PinchDemoView()
+        case .waveInterference:
+            WaveInterferenceDemoView()
+        case .lumaKey:
+            LumaKeyDemoView()
+        case .bulge:
+            BulgeDemoView()
+        case .digitalStatic:
+            DigitalStaticDemoView()
+        case .circleReveal:
+            CircleRevealDemoView()
+        case .sepia:
+            SepiaDemoView()
+        case .snowfall:
+            SnowfallDemoView()
+        case .polarWarp:
+            PolarWarpDemoView()
+        case .lightLeak:
+            LightLeakDemoView()
+        case .colorQuantizationGlitch:
+            ColorQuantizationGlitchDemoView()
+        case .godRays:
+            GodRaysDemoView()
+        case .flagDistortion:
+            FlagDistortionDemoView()
+        case .analogStatic:
+            AnalogStaticDemoView()
+        case .stuckPixels:
+            StuckPixelsDemoView()
+        case .terminalGlitch:
+            TerminalGlitchDemoView()
+        case .dotMatrix:
+            DotMatrixDemoView()
+        case .signalNoise:
+            SignalNoiseDemoView()
+        case .metaballs:
+            MetaballsDemoView()
+        case .posterize:
+            PosterizeDemoView()
+        case .radialWipe:
+            RadialWipeDemoView()
+        case .lineDropout:
+            LineDropoutDemoView()
+        case .levels:
+            LevelsDemoView()
+        case .ghosting:
+            GhostingDemoView()
+        case .doubleVision:
+            DoubleVisionDemoView()
+        case .horizontalTear:
+            HorizontalTearDemoView()
+        case .compressionArtifacts:
+            CompressionArtifactsDemoView()
+        case .chromaKey:
+            ChromaKeyDemoView()
+        case .fisheye:
+            FisheyeDemoView()
+        case .deadPixels:
+            DeadPixelsDemoView()
+        case .verticalTear:
+            VerticalTearDemoView()
+        case .kaleidoscope:
+            KaleidoscopeDemoView()
+        case .glitchMosaic:
+            GlitchMosaicDemoView()
+        case .heatHaze:
+            HeatHazeDemoView()
+        case .sliceGlitch:
+            SliceGlitchDemoView()
+        case .colorBalance:
+            ColorBalanceDemoView()
+        case .toonShade:
+            ToonShadeDemoView()
+        case .vhsHeadSwitch:
+            VHSHeadSwitchDemoView()
+        case .pixelate:
+            PixelateDemoView()
+        case .vibrance:
+            VibranceDemoView()
+        case .noiseDissolve:
+            NoiseDissolveDemoView()
+        case .interlacing:
+            InterlacingDemoView()
+        case .lensRefraction:
+            LensRefractionDemoView()
+        case .displacementTransition:
+            DisplacementTransitionDemoView()
+        case .saturation:
+            SaturationDemoView()
+        case .mirror:
+            MirrorDemoView()
+        case .temperature:
+            TemperatureDemoView()
+        case .bitCrush:
+            BitCrushDemoView()
+        case .outline:
+            OutlineDemoView()
+        case .threshold:
+            ThresholdDemoView()
+        case .fragmentShuffle:
+            FragmentShuffleDemoView()
+        case .twirl:
+            TwirlDemoView()
+        case .thermalVision:
+            ThermalVisionDemoView()
+        case .frameJitter:
+            FrameJitterDemoView()
+        case .crtPhosphorMask:
+            CRTPhosphorMaskDemoView()
+        case .linearWipe:
+            LinearWipeDemoView()
+        case .waterCaustics:
+            WaterCausticsDemoView()
+        case .crtCurvature:
+            CRTCurvatureDemoView()
+        case .brokenLCD:
+            BrokenLCDDemoView()
         }
     }
 }
